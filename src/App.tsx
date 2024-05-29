dc3a9cbc3cf8aa583f15e1c0c4c711a2d321e5b2
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-29 21:07:57 +0000

    Passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App(){
-   const cities = [
-    'Accra',
-    'Pretoria',
-    'Dodoma',
-    'Mombasa',
-    'Tripoli',
-    'Lagos',
-  ]
-
-  const countries = [
-    'Ghana',
-    'South Africa',
-    'Tanzania',
-    'Kenya',
-    'Libya',
-    'Nigeria',
-  ]
-
-  const handleSelectItem = (item:string) =>{
-    console.log(item);
-    
-  }
-
   return (
     <div>
-        <ListGroup items={countries} heading='Countries'onSelectItem={handleSelectItem}/>
-        <ListGroup items={cities} heading='Cities'onSelectItem={handleSelectItem}/>
+      <Alert>
+        Hey there world
+      </Alert>
     </div>
   ) 
 }
