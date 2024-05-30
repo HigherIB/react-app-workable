ded3f9733ac85eb0188229be9398366886535f8d
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-30 09:06:07 +0000

    alert visibility final

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App(){
+  const [alertVisible, setAlertVisibility] = useState(false)
   return (
     <div>
+      {alertVisible && 
       <Alert>
         check this alert
-      </Alert>
+      </Alert>}
       
       <Button />
     </div>
