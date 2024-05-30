ab6faa23803612849edb5922fefe6e55bcef3ff7
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-30 16:35:43 +0000

    state toggle

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -10,7 +10,7 @@ const ExpandableText = ({ children, onClick, maxChar = 100} : Props) => {
     const [isExpandable, setIsExpandable] = useState(false)
         if (children.length <= maxChar) return <p>{children}</p>;
          const text = children.substring(0, maxChar);
-         return <p>{text}... <button> {isExpandable ? 'Less' : 'More'} </button> </p>
+         return <p>{text}... <button type="button" onClick={() => setIsExpandable(!isExpandable)}> {isExpandable ? 'Less' : 'More'} </button> </p>
 }
 
 export default ExpandableText
