bc5fcb40a59da2f59279ce50bbd574cce49fe769
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-30 08:48:29 +0000

    check this alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App(){
   return (
     <div>
+      <Alert>
+        check this alert
+      </Alert>
+      
       <Button />
     </div>
   ) 
