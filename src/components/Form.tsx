b7c05d893fbeddb9793210566823234ff1759edb
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-30 19:29:04 +0000

    useRef hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,25 @@
-import React from "react"
+import React, { FormEvent,useRef } from "react"
 
 const Form = () => {
+    const nameRef = useRef(null);
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        if (nameRef !== null)
+        console.log('nameRef.current.value');
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" type="text" className="form-control" />
+                <input ref={nameRef} id="name" type="text" className="form-control" />
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Name</label>
                 <input id="age" type="number" className="form-control" />
             </div>
-            <button className="btn btn-primary">Submit</button>
+            <button className="btn btn-primary" type="submit">Submit</button>
         </form>
     )
 }
