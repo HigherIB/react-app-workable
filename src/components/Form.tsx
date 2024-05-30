247410a8f2627d2744d603ac3279532f2e8c9c12
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-30 18:55:51 +0000

    simple debuging

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,13 +2,18 @@ import React from "react"
 
 const Form = () => {
     return (
-        <form action="">
-            <div className="mb-3">
-                <lable htmlFor="name" className="form-lable"></lable>
-                <input id="name" type="text" className="form-control" />
+        <form>
+            <div>
+                <label htmlFor="name"></label>
+                <input id="name" type="text" />
             </div>
         </form>
     )
 }
+
+{/* <div className="mb-3">
+<lable htmlFor="name" className="form-lable"></lable>
+<input id="name" type="text" className="form-control" />
+</div> */}
 
 export default Form
