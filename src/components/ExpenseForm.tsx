73481b888fdbb9f016036212b9355031615e6aae
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-31 14:06:38 +0000

    zod validation

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,38 +4,58 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-    descrption: z.string().min(3, {message: 'Description must be atleast 3 characters'}),
+    description: z.string().min(3, {message: 'Description must be atleast 3 characters'}),
     amount: z.number({ invalid_type_error: 'Amount is required' }).min(1),
-    category: z.string().min(3)
+    category: z.string({ invalid_type_error: 'Category is required' }).min(3)
 })
 
+type FormData = z.infer<typeof schema>
 
 const ExpenseForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) })
     return (
         <form>
-          <label htmlFor="description" className="form-label">Description</label>
-          <input 
-                id="description"
-                type="text"
-                className="form-control"
-            />
-          <label htmlFor="amount" className="form-label">Amount</label>
-          <input 
-                id="amount"
-                type="number"
-                className="form-control"
-            />
-            <label htmlFor="category" className="form-label">Category</label>
-            <select 
-                name="categories" 
-                id="category"
-                className="form-select" 
-            >
-                <option value="groceries">Groceries</option>
-                <option value="utilities">Utilities</option>
-                <option value="entertainment">Entertainment</option>
-            </select>
-            <button type="submit" className="btn btn-primary mt-3">Submit</button>
+         <div className="mb-3">
+             <label htmlFor="description" className="form-label">Description</label>
+              <input
+                    { ...register('description') } 
+                    id="description"
+                    type="text"
+                    className="form-control"
+                />
+                { errors.description && (
+                    <p className="text-danger">{ errors.description.message }</p>
+                ) }
+         </div>
+         <div className="mb-3">
+             <label htmlFor="amount" className="form-label">Amount</label>
+              <input
+                    { ...register('amount') }
+                    id="amount"
+                    type="number"
+                    className="form-control"
+                />
+                { errors.amount && (
+                    <p className="text-danger">{ errors.amount.message }</p>
+                ) }
+         </div>
+         <div className="mb-3">
+             <label htmlFor="category" className="form-label">Category</label>
+                <select
+                    { ...register('category') }
+                    name="categories" 
+                    id="category"
+                    className="form-select" 
+                >
+                    <option value="groceries">Groceries</option>
+                    <option value="utilities">Utilities</option>
+                    <option value="entertainment">Entertainment</option>
+                </select>
+                { errors.category && (
+                    <p className="text-danger">{ errors.category.message }</p>
+                ) }
+         </div>
+         <button type="submit" className="btn btn-primary mt-3">Submit</button>
         </form>
     )
 }
