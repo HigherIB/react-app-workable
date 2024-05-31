2320108faf91a76efaf4763c11f72f1398ccb4f0
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-31 13:11:07 +0000

    form bootstrap styles

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,21 +1,25 @@
 const ExpenseForm = () => {
     return (
         <form>
-          <label htmlFor="description">Description</label>
+          <label htmlFor="description" className="form-label">Description</label>
           <input 
                 id="description"
                 type="text"
+                className="form-control"
             />
-          <label htmlFor="amount">Amount</label>
+          <label htmlFor="amount" className="form-label">Amount</label>
           <input 
                 id="amount"
                 type="number"
+                className="form-control"
             />
-            <label htmlFor="category">Category</label>
+            <label htmlFor="category" className="form-label">Category</label>
             <input 
                 id="category"
                 type="text"
+                className="form-control"
             />
+            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
 }
