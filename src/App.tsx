2c21f62d38c94c9c6454e2802fa215541ec53eda
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-31 17:23:53 +0000

    expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react"
-import ExpenseForm from "./components/ExpenseForm"
-import ExpenseSummary from "./components/ExpenseSummary"
+import ExpenseList from "./expense-tracker/components/ExpenseList"
+import ExpenseForm from "./expense-tracker/components/ExpenseForm"
+import ExpenseSummary from "./expense-tracker/components/ExpenseSummary"
 
 
 function App(){
+  const expenses = [
+    {id: 1, description: 'aaa', amount: 100, category: 'Groceries'}
+    {id: 2, description: 'sss', amount: 100, category: 'Utilities'}
+    {id: 3, description: 'kkk', amount: 100, category: 'Groceries'}
+    {id: 4, description: 'jjj', amount: 100, category: 'Entertainment'}
+    {id: 5, description: 'zzz', amount: 100, category: 'Groceries'}
+
+  ]
+
 
   return (
     <div>
       <ExpenseForm />
       <ExpenseSummary />
+      <ExpenseList expenses={expenses} onDelete={() => {}}  />
     </div>
   ) 
 }
