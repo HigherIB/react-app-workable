1741cb66887762a97e5e232a4b9c32a718498a62
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-01 17:03:26 +0000

    aborting fetch and loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { CanceledError } from "axios"
 import React, { useEffect, useRef, useState } from "react"
 
 interface User {
@@ -10,17 +10,34 @@ interface User {
 
 function App(){
   const [users, setUsers] = useState<User[]>([])
-
+  const [error, setError] = useState('')
+  const [isLoading, setLoading] = useState(false)
   // After render
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(res => setUsers(res.data))
+    const controller = new AbortController();
+
+    setLoading(true);
+
+    axios.get("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
+      .then(res => {
+        setUsers(res.data);
+        setLoading(false)
+      })
+      .catch(err => {
+        if(err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      })
+
+    return () => controller.abort();
   }, [])
 
   
 
   return (
     <div>
+      {isLoading && <div className="spinner-border">Loading...</div className=""> }
+      {error && <p className="text-danger">{error}</p> }
       <ul className="list-group">
         {users.map(user => <li key={user.id} className="list-group-item">{user.name}</li> )}
       </ul>
