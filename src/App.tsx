34f6be551e864543b5dd0fb88c669b66cf8f632b
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-01 16:21:22 +0000

    axios fetch practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
+import axios from "axios"
 import React, { useEffect, useRef, useState } from "react"
-import ProductList from "./components/ProductList";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 
 
-function App(){
-  const ref = useRef<HTMLInputElement>(null);
-  const [category, setCategory] = useState('')
+function App({id, name}: User){
+  const [users, setUsers] = useState<User[]>([])
 
   // After render
   useEffect(() => {
-    if (ref.current) ref.current.focus();
-  })
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => setUsers(res.data))
+  }, [])
 
-  useEffect(() => {
-    document.title = 'My App';
-  })
+  
 
   return (
     <div>
-      <select name="categories" id="categories" className="form-select" onChange={(e) => setCategory(e.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">HouseHold</option>
-      </select>
-      <ProductList category={category} />
-      <input ref={ref} type="text" className="form-control" />
+      <ul className="list-group">
+        {users.map(user => <li key={user.id} className="list-group-item">{user.name}</li> )}
+      </ul>
     </div>
   ) 
 }
