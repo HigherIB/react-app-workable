4222ebf8e08d352b52fb3c070839da50dae3fdc8
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-01 17:21:48 +0000

    implimenting optimistic deletion

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,17 @@ function App(){
     return () => controller.abort();
   }, [])
 
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+
+    setUsers(users.filter((user) => user !== user));
+
+    axios.delete("https://jsonplaceholder.typicode.com/users" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  }
   
 
   return (
@@ -39,7 +50,8 @@ function App(){
       {isLoading && <div className="spinner-border"></div> }
       {error && <p className="text-danger">{error}</p> }
       <ul className="list-group">
-        {users.map(user => <li key={user.id} className="list-group-item d-flex justify-content-between">{user.name} <button className="btn btn-outline-danger">Delete</button> </li> )}
+        {users.map(user => <li key={user.id} className="list-group-item d-flex justify-content-between">{user.name}
+         <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button> </li> )}
       </ul>
     </div>
   ) 
