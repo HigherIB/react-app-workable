053e5c13d00bfcde716d8f130944ed22d3a5ea9a
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-03 10:14:45 +0000

    custom hooks useUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React, { useEffect, useRef, useState } from "react"
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App(){
-  const [users, setUsers] = useState<User[]>([])
-  const [error, setError] = useState('')
-  const [isLoading, setLoading] = useState(false)
-
-
-  // After render
-  useEffect(() => {
-    setLoading(true);
-
-    const { request, cancel } = userService.getAll<User>();
-      request
-        .then(res => {
-          setUsers(res.data);
-          setLoading(false)
-        })
-        .catch(err => {
-          if(err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        })
-
-    return cancel();
-  }, [])
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const addUser = () => {
     const originalUsers = [...users];
@@ -68,7 +47,7 @@ function App(){
 
   return (
     <div>
-      {isLoading && <div className="spinner-border"></div> }
+      {isLoading && <div className="spinner-border mx-5 my-5"></div> }
       {error && <p className="text-danger">{error}</p> }
       <button type="button" className="btn btn-primary my-2" onClick={() => addUser}>Add New User</button>
       <ul className="list-group">
